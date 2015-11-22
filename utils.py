fd64a19b3b2d726d38ea50e9c22684e210c38323
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-11-22 15:48:27 +0530

    script

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import nltk
 from nltk import *
 from stanford_corenlp_pywrapper import CoreNLP
 
+parser = CoreNLP("parse", corenlp_jars=[os.getcwd() + "/stanford-corenlp-full-2015-04-20/*"])
+# ner_extracter = CoreNLP("ner", corenlp_jars=[os.getcwd() + "/stanford-corenlp-full-2015-04-20/*"])
+
 def sen_segment(data):
     """Returns a List of Sentences segmented from test data"""
 
@@ -17,35 +20,18 @@ def word_tokenize(data):
 def dep_parse(data):
     """Creates dependency parser sentence by sentence for given data
     sequence."""
-
-    proc = CoreNLP("parse", corenlp_jars=[os.getcwd() + "/stanford-corenlp-full-2015-04-20/*"])
-    parse_res = proc.parse_doc(data)
+    parse_res = parser.parse_doc(data)
+    # print parse_res
     deptree_list = []
-    for each in parse_res:
+    for each in parse_res["sentences"]:
         deptree_list.append(each['parse'])
     return deptree_list
 
 def ner_extract(data):
-    
-    proc = CoreNLP("ner", corenlp_jars=[os.getcwd() + "/stanford-corenlp-full-2015-04-20/*"])
-    parse_res = proc.parse_doc(data)
-    ner_list = []
-    for each in parse_res:
-        ner_list.append(each['ner'])
-    return ner_list
-
-def filter_ner(ner_list, token_list):
-
-    token_dic = {}
-    for each_ner in ner_list:
-        if (each_ner == u'O'):
-
-
-
-def extract_feature(data):
-    """Created tuples and add features to the corr
-    list element."""
-
-    for each in ner_extract(data):
-
+    parse_res = ner_extracter.parse_doc(data)
+    print parse_res
+    # ner_list = []
+    # for each in parse_res:
+        # ner_list.append(each['ner'])
+    # return ner_list
 
